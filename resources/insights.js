161218675881a8079c0d7b41aae3f38cf84eb1d7
Adrian Moldovan <adim.moldovan@gmail.com>
2020-09-26 16:24:00 +0300

    Cleaned up js

--- resources/insights.js
+++ resources/insights.js
@@ -1,41 +1,56 @@
-let coll = document.getElementsByClassName("clipboard");
-    let i;
+// region clipboard
+let clipboardCells = document.getElementsByClassName("clipboard");
 
-    for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function () {
-            ta = document.createElement('textarea');
-            ta.value = this.innerText;
-            ta.setAttribute('readonly', '');
-            ta.style.position = 'absolute';
-            ta.style.left = '-9999px';
-            document.body.appendChild(ta);
-            ta.select();
-            document.execCommand('copy');
-            document.body.removeChild(ta);
-        });
-    }
-
-let options = {
-        valueNames: ['name', 'failureRate']
-    };
-
-    let specList = new List('specs', options);
-
-    let filterNoFailuresChkBx = document.getElementById("filterNoFailures")
-    filterNoFailuresChkBx.addEventListener('change', function () {
-        filterByFailuresRate()
+Array.from(clipboardCells).forEach(function (item) {
+    item.addEventListener("click", function () {
+        let textAreaEl = document.createElement('textarea');
+        textAreaEl.value = this.innerText;
+        textAreaEl.setAttribute('readonly', '');
+        textAreaEl.style.position = 'absolute';
+        textAreaEl.style.left = '-9999px';
+        document.body.appendChild(textAreaEl);
+        textAreaEl.select();
+        document.execCommand('copy');
+        document.body.removeChild(textAreaEl);
     });
+});
 
-    specList.sort('failureRate', {order: "desc"});
+// endregion
+
+// region define list options
+let options = {
+    valueNames: ['name', 'failureRate']
+};
+
+let specList = new List('specs', options);
+
+// endregion
+
+// region filters
+// Filter out cases with 0 failure rate
+let filterNoFailuresChkBx = document.getElementById("filterNoFailures")
+filterNoFailuresChkBx.addEventListener('change', function () {
     filterByFailuresRate()
+});
 
-    function filterByFailuresRate() {
-        specList.filter(function (item) {
-            let failureRate = parseInt(item.values()["failureRate"]);
-            if (filterNoFailuresChkBx.checked) {
-                return failureRate > 0;
-            } else {
-                return failureRate >= 0;
-            }
-        });
-    }
+// endregion
+
+// region default options
+specList.sort('failureRate', {order: "desc"});
+filterByFailuresRate()
+// endregion
+
+/*
+* Filters listed test cases based in failure rate and the state of the control checkbox
+* Checkbox checked => filter out test cases with 0 failure rate
+* */
+function filterByFailuresRate() {
+    specList.filter(function (item) {
+        let failureRate = parseInt(item.values()["failureRate"]);
+        if (filterNoFailuresChkBx.checked) {
+            return failureRate > 0;
+        } else {
+            return failureRate >= 0;
+        }
+    });
+}
